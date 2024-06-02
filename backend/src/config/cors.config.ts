923ef274fa33ec6b78f154dbe292324d22b15e1a
hamilgdev <hamilgdev@gmail.com>
2024-06-02 15:52:33 -0500

    refactor: Update allowed origins in cors.config.ts

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,14 +1,15 @@
 import { EnvConfig } from './env.config';
 
-let allowedOrigins: (RegExp | string)[] = [/localhost:\d{4200}$/];
+let allowedOrigins: (RegExp | string)[] = [/localhost:\d{4}$/];
 
 export const corsConfig = {
   origin: allowedOrigins,
-  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Authorization', 'X-Requested-With', 'Content-Type'],
   maxAge: 86400, // NOTICE: 1 day
   credentials: true,
 };
 
-if (EnvConfig().cors_origin)
+if (EnvConfig().cors_origin) {
   allowedOrigins = allowedOrigins.concat(EnvConfig().cors_origin.split(','));
+}
